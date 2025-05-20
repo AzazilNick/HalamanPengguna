5065d7663c84e133ea2c56af7b7e2c3ced5bc75f
Nick_Wilsan <72246382+AzazilNick@users.noreply.github.com>
2025-05-21 00:17:33 +0700

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="StyleDashboard.css">
+    <link rel="stylesheet" href="style_dashboard.css">
 </head>
 <body>
     <header>

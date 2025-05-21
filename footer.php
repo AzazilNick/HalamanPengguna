4e14d179d15b27d0268b60a8a96cfb98317ad49e
Nick_Wilsan <72246382+AzazilNick@users.noreply.github.com>
2025-05-21 08:42:13 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,20 +4,7 @@
         <p>&copy; 2025 Movie & Series Review</p>
     </footer>
 
-    <script>
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector(".nav-menu");
-    const mainContent = document.querySelector("main");
-
-    menuToggle.addEventListener("click", () => {
-        navMenu.classList.toggle("active");
-        if (navMenu.classList.contains("active")) {
-            mainContent.style.marginTop = "100px";
-        } else {
-            mainContent.style.marginTop = "0px";
-        }
-    });
-    </script>
+    <script src="script.js"></script>
 
 </body>
 </html>

a89089030a61386aa5cdefc344cd09de5c07e2c1
Nick_Wilsan <72246382+AzazilNick@users.noreply.github.com>
2025-06-04 08:22:45 +0700

    Update index.php

--- app/Views/articles/index.php
+++ app/Views/articles/index.php
@@ -24,11 +24,7 @@ if ($basePath === '/') {
             <div class="notification <?= escape_html($message_type ?? '') ?>"><?= escape_html($message) ?></div>
         <?php endif; ?>
 
-        <?php if (Session::has('user')): // Hanya tampilkan tombol buat artikel jika sudah login ?>
-            <p><a href="<?= $basePath ?>/articles/create" class="btn btn-create-article">Buat Artikel Baru</a></p>
-        <?php else: ?>
-            <p class="info-message">Login untuk membuat artikel baru.</p>
-        <?php endif; ?>
+        <p><a href="<?= $basePath ?>/articles/create" class="btn btn-create-article">Buat Artikel Baru</a></p>
 
 
         <?php if (!empty($articles)): ?>
